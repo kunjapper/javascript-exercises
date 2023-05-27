619caed593214d92280beec51c613a09d3cc8a19
Anson K <122587302+kunjapper@users.noreply.github.com>
2023-05-27 00:22:55 -0500

    exercise 2 repeat strings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const { text } = require("express");
 
+const repeatString = function repeat(text, count){
+    var newString="";
+    console.log("your original text is "+text);
+    
+    if(text<0){
+        return ERROR;
+    }
+    newString = text;
+    for(var i=0; i<=count; i++);{
+        console.log("loop number"+i);
+        newString = newString.concat("",newString);
+    }
+    console.log("Your new text is " + newString);
+    return newString;
 };
 
 // Do not edit below this line
