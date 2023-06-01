faa38f16732a478a065a442d53a41fb2ebf07ab0
Anson K <122587302+kunjapper@users.noreply.github.com>
2023-06-01 00:02:09 -0500

    lesson 4 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(original, ...argument) {
+    /*
+    the original thing i tried which is too complicated
+    var x = original.length;
+    console.log(x);
 
+    for (let i=0; i++, i<x;){
+        console.log(i);
+        if(original[i]==argument)
+        original.splice(i);
+    }
+    console.log(original);
+    return original;
+    */
+
+    const newArray = [];
+
+    original.forEach((item) =>{
+        if(!argument.includes(item)){
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
