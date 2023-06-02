350aaa5c9f5b2616a8d434f43ccb0012a93f177d
Anson K <122587302+kunjapper@users.noreply.github.com>
2023-06-02 12:08:06 -0500

    working on ex 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  var convertedF = (tempF * (9/5)+(32)).toFixed(2);
+  return convertedF;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  var convertedC = ((5/9)*(tempF-32)).toFixed(2);
+  return convertedC;
 };
 
 // Do not edit below this line
