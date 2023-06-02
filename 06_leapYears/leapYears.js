9e3b18bf751447f94892c748f07f7b1cef34d2bc
Anson K <122587302+kunjapper@users.noreply.github.com>
2023-06-02 11:52:56 -0500

    lesson 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    var isLeapYear = false;
+
+    //leap year is divisible by 4
+    //if year is divisible by 100 it isnt a leap year unless its divisible by 400 as well
+
+    
+
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+
+    
 
 };
 
